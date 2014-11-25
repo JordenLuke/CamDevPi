115277de50c12f6fc271bac8bb10b2a368a7d986
Jorden Luke <jorden.luke@gmail.com>
2014-11-25 07:48:30 +0000

    Added the mt9m034Pi.h to make used in other progams

---
+++ mt9m034Pi.h
@@ -0,0 +1,21 @@
+#ifndef MT9M034PI_H
+#define MT9M034PI_H
+
+
+//structed used to hold some data
+struct mt9m034_data{
+	struct mt9m034_pll_divs *pll;
+	struct pin *reset;
+	int fd;
+	int version;
+};
+//used to start  the  i2c and used
+int mt9m034_init(struct mt9m034_data *mt9m034, int pin);
+//can be used as rest
+void mt9m034_power_on(struct mt9m034_data *mt9m034);
+//toggle streaming
+static int mt9m034_s_stream(struct mt9m034_data *mt9m034, int enable);
+
+
+
+#endif
--- testcam.c
+++ testcam.c
@@ -5,13 +5,19 @@
 #define START 4
 #define SNAP  5
 #define RESET 1
+#define DONE  6
 
 int gpio_setup();
-
+void myInterupt();
 
 int main(void)
 {
-	gpio_setup();
+	if( gpio_setup()  < 0)
+	{
+		printf("ERROR: Failed on gpio init \n");
+		return 1;
+
+	}
 
 	printf("Welcome to the MT9M034 test software \n");
 	printf("Press button to start test \n");
@@ -25,14 +31,27 @@ int main(void)
 int gpio_setup()
 {
 
+	if (wiringPiSetup () < 0)
+  	{
+    		fprintf (stderr, "Unable to setup wiringPi: %s\n", strerror (errno)) ;
+    		return 1 ;
+  	}
 
-	if( wiringPiSetup() == -1)
-	return -1;
+  	if (wiringPiISR (DONE, INT_EDGE_FALLING, &myInterrupt) < 0)
+  	{
+    		fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
+    		return 1 ;
+  	}
 
 	pinMode(START,INPUT);
 	pinMode(SNAP,INPUT);
 	pullUpDnControl (START,PUD_UP);
 	pullUpDnControl (SNAP,PUD_UP);
-	return 0;
+	return 1;
+}
+void myInterupt()
+{
+
+	printf("Picture Captured \n");
 
 }
