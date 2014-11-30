6d19594f6eba071e3d5f1942cd784b6e6d455574
Jorden Luke <jorden.luke@gmail.com>
2014-11-30 09:02:22 +0000

    fixed error with random crash added serial functionality changed operation a bit

--- testcam.c
+++ testcam.c
@@ -1,10 +1,11 @@
 /*
-* Complie :gcc -o testcam  testcam.c mt9m034Pi.c -lwiringPi -lpthread -lm
+* Complie : gcc -o testcam testcam.c  -I. mt9m034Pi.o -lwiringPi -lpthread
 *
 *
 */
 #include <wiringPi.h>
 #include "mt9m034Pi.h"
+#include <wiringSerial.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -16,31 +17,68 @@
 #define RESET 1
 #define DONE  6
 
+
+struct serial{
+ int fd;
+ int baud;
+ char  *name;
+};
+
+struct mem_cmd{
+ char add[4];
+ char *data;
+ int size;
+};
+
 void myInterrupt();
-int gpio_setup();
+int gpio_setup(struct serial *dev);
+int init_serial(struct serial *dev);
 
-struct mt9m034_data *mt9m034;
-
-int main(void)
+struct mt9m034_data mt9m034;
+struct pin reset;
+int flag = 0x00;
+//serial device name and baud
+int main(int argc,char *args[])
 {
-	if( gpio_setup()  < 0)
+	struct serial dev;
+	struct mem_cmd mem;
+	int err;
+
+	if(argc > 1)
+	{
+		memcpy(dev.name, (args[1]+1), sizeof(args[1]-1);
+		dev.baud = atoi((args[2]+1));
+	}
+	else
+	{
+		dev.name = "/dev/tty/AMA0";
+		dev.baud = 115200;
+	}
+
+	mt9m034.reset = reset;
+	if( gpio_setup(&dev)  < 0)
 	{
 		printf("ERROR: Failed on gpio init \n");
 		fflush(stdout);
 		return 1;
-
 	}
 
 	printf("Welcome to the MT9M034 test software \n");
 	printf("Press button to start test \n");
-	while(digitalRead(START));
-	printf("Starting Camera \n");
-	while(digitalRead(SNAP));
-	printf("Taking Snap Shot \n");
+	for(;;)
+	{
+		while(digitalRead(START));
+
+		mt9m034_s_stream(&mt9m034,1);
+		printf("Starting Camera \n");
+		while(!flag);
+		while(digitalRead(SNAP));
+		printf("Taking Snap Shot \n");
+	}
 
 	return 0;
 }
-int gpio_setup()
+int gpio_setup(struct serial *dev)
 {
 
 	if (wiringPiSetup () < 0)
@@ -54,11 +92,17 @@ int gpio_setup()
    		printf ("Unable to setup ISR: \n") ;
     		return -1 ;
   	}
-	if(mt9m034_init(mt9m034, RESET)  < 0 )
+	if(mt9m034_init(&mt9m034, RESET)  < 0 )
 	{
 		printf ("Unable to init camera:  \n") ;
 		return -1;
 	}
+	if ((dev->fd = serialOpen (dev->name, dev->baud)) < 0)
+        {
+                fprintf (stderr, "Unable to open serial device: %s\n", strerror (errn$
+                return 1 ;
+        }
+
 
 	pinMode(START,INPUT);
 	pinMode(SNAP,INPUT);
@@ -70,5 +114,15 @@ void myInterrupt()
 {
 
 	printf("Picture Captured \n");
+	mt9m034_s_stream(&mt9m034,0);
+	flag = 0x01;
+}
+int init_serial(struct serial *dev){
 
+	if ((dev->fd = serialOpen (dev->name, dev->baud)) < 0)
+  	{
+    		fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+    		return 1 ;
+  	}
+	return 0;
 }
